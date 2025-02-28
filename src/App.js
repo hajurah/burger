9a502072e8e0f7c905aad75265eb91e78505dc7e
hajura <hajurah2000@gmail.com>
2025-02-28 10:04:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 // import React from "./react";
-import './App.css';
- import Header from "./Component/Header";
- import Home from "./Component/Home";
- import "reactstrap";
- import "react-bootstrap";
- import "bootstrap/dist/css/bootstrap.min.css";
-import Feedback from './Component/Feedback';
-// import Footar from "./component/Footar";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // Ensure this is imported first
+import React from "react";
+import "./App.css";
+import Header from "./Component/Header";
+import Home from "./Component/Home";
+import Feedback from "./Component/Feedback";
 
+// import Footar from "./component/Footar";
 
 function App() {
   return (
     <>
       <Header />
-      <Home /> 
+      <Home />
       <Feedback />
-
-
     </>
   );
 }
